b4cd4cf36dd1512e940e05132cf193cd0c0cea49
nlavanya-tech <nlavanya987@gmail.com>
2020-06-26 17:01:43 +0530

    initial commit or whatever message for first commit

---
+++ NoteApplicationApi.DataLayer/MongoDbSetting.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NoteApplicationApi.DataLayer
+{
+    public class MongoDbSetting
+    {
+        public string ConnectionString { get; set; }
+        public string Database { get; set; }
+    }
+}
